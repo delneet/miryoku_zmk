30d3f6a8ced2e981e7cfc51515bd66a81ecdbac0
Daniel D. <danieldelnaaij@gmail.com>
2025-05-08 14:02:01 +0200

    fix: Redifine layer numbers

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -10,10 +10,10 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 
 
 #define MIRYOKU_LAYER_DEV \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LPAR,          &kp RPAR,          &kp EQUAL,         &kp MINUS,         &kp PIPE,          \
+U_NA,              U_NA,              &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LPAR,          &kp RPAR,          &kp EQUAL,         &kp MINUS,         &kp PIPE,          \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LBKT,          &kp RBKT,          &kp AMPS,          &kp CARET,         &kp COLON,         \
 U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_SYM,       U_NA,              &kp LBRC,          &kp RBRC,          &kp LT,            &kp GT,            &kp TILDE,         \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp UNDER,         U_NA,              U_NA,              U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp UNDER,         &kp BSPC,          U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
@@ -35,10 +35,9 @@ MIRYOKU_X(DEV,    "Dev")
 #define U_TAP    2
 #define U_BUTTON 3
 #define U_NAV    4
-#define U_DEV    5
+#define U_MOUSE  5
 #define U_MEDIA  6
 #define U_NUM    7
 #define U_SYM    8
 #define U_FUN    9
-#define U_MOUSE U_NAV
-
+#define U_DEV    10
